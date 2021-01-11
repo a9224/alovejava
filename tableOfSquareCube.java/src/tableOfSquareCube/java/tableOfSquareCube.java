124021261a905b773c64dc7a4849ec3f6aec1d34
cpe9210 <cpe9210@users.noreply.github.com>
2021-01-11 04:42:41 +0300

    done

---
+++ tableOfSquareCube.java/src/tableOfSquareCube/java/tableOfSquareCube.java
@@ -0,0 +1,57 @@
+package tableOfSquareCube.java;
+
+public class tableOfSquareCube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n,s,c;
+		System.out.println("number\tsquare\tcube");
+		n=0;
+		s=n*n;
+		c=n*n*n;
+		System.out.printf("%d\t%d\t%d\n",n,s,c);
+		n=1;
+		s=n*n;
+		c=n*n*n;
+		System.out.printf("%d\t%d\t%d\n",n,s,c);
+		n=2;
+		s=n*n;
+		c=n*n*n;
+		System.out.printf("%d\t%d\t%d\n",n,s,c);
+		n=3;
+		s=n*n;
+		c=n*n*n;
+		System.out.printf("%d\t%d\t%d\n",n,s,c);
+		n=4;
+		s=n*n;
+		c=n*n*n;
+		System.out.printf("%d\t%d\t%d\n",n,s,c);
+		n=5;
+		s=n*n;
+		c=n*n*n;
+		System.out.printf("%d\t%d\t%d\n",n,s,c);
+		n=6;
+		s=n*n;
+		c=n*n*n;
+		System.out.printf("%d\t%d\t%d\n",n,s,c);
+		n=7;
+		s=n*n;
+		c=n*n*n;
+		System.out.printf("%d\t%d\t%d\n",n,s,c);
+		n=8;
+		s=n*n;
+		c=n*n*n;
+		System.out.printf("%d\t%d\t%d\n",n,s,c);
+		n=9;
+		s=n*n;
+		c=n*n*n;
+		System.out.printf("%d\t%d\t%d\n",n,s,c);
+		n=10;
+		s=n*n;
+		c=n*n*n;
+		System.out.printf("%d\t%d\t%d\n",n,s,c);
+		
+		
+	}
+
+}
